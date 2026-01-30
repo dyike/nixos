becd39f7edae990729289031293f50c08369a4a2
dyike <yuanfeng634@gmail.com>
2026-01-30 22:11:18 +0800

    update users

--- modules/users.nix
+++ modules/users.nix
@@ -4,9 +4,6 @@
   users.users.yuan = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
-    # openssh.authorizedKeys.keys = [
-    #   "ssh-ed25519 AAAA... your_pubkey"
-    # ];
   };
   programs.zsh.enable = true;
 
